991a319c8c776b37b8d31ab7af265d5c5485c22d
krodante <kathy.rodante@gmail.com>
2023-05-12 08:48:01 -0700

    cache by zipcode

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -7,16 +7,13 @@ class WeatherController < ApplicationController
 
   def show
     @read_from_cache = true
-    encoded_location = Base64.encode64(location_params[:text])
-    cached_weather = Rails.cache.fetch(encoded_location, expires_in: 1.minute) do
-      @read_from_cache = false
-      location = Location.new(location_params).set_coordinates
-      weather_api = Weather.new(location.lat, location.lon)
-      weather_api.cache_json
+    cached_weather = read_or_write_cache
+    if cached_weather
+      @weather_current = cached_weather[:current_forecast]
+      @weather_forecast = cached_weather[:five_day_forecast].group_by { |forecast| Time.zone.at(forecast.time).to_date }
+    else
+      redirect_to action: "index"
     end
-
-    @weather_current = cached_weather[:current_forecast]
-    @weather_forecast = cached_weather[:five_day_forecast].group_by { |forecast| Time.zone.at(forecast.time).to_date }
   end
 
   private
@@ -24,4 +21,22 @@ class WeatherController < ApplicationController
   def location_params
     params.require(:location).permit(:text)
   end
+
+  def parse_zipcode(text)
+    text.match(/\d{1,5}/).to_s
+  end
+
+  def read_or_write_cache
+    zipcode = parse_zipcode(location_params[:text])
+
+    return unless zipcode.present?
+
+    Rails.cache.fetch(zipcode, expires_in: 1.minute) do
+      @read_from_cache = false
+      location = Location.new(location_params).set_coordinates
+      weather_api = Weather.new(location.lat, location.lon)
+      weather_api.cache_json
+    end
+  end
+
 end
